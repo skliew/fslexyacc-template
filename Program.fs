906113affb339e57c4c20008a8f13d3aaa679d80
skliew <skliew@gmail.com>
2021-08-20 15:54:38 +0800

    First commit

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Text.Lexing
+
+[<EntryPoint>]
+let rec main argv =
+  printf "calc> "
+  let input = System.Console.ReadLine()
+  let lexbuf = LexBuffer<char>.FromString input
+  try
+    let result = Parser.start Lexer.token lexbuf
+    printfn "result: %A" result
+  with
+    | ex -> printfn "error: %A" ex
+  main argv
+  0
